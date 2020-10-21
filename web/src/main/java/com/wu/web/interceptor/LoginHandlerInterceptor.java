210a6e9ce4bd786ec5c1548f11b49255c83496ea
wuhjie <whjie001110@outlook.com>
2020-10-22 00:18:13 +0800

    10.21

---
+++ web/src/main/java/com/wu/web/interceptor/LoginHandlerInterceptor.java
@@ -0,0 +1,9 @@
+package com.wu.web.interceptor;/**
+  *@ClassName LoginHandlerIntercepter
+  *@Description 
+  *@Author wuhjie
+  *@Data 2020/10/20 4:20 pm
+  *@Version 1.0
+  **/
+public class LoginHandlerIntercepter {
+}
